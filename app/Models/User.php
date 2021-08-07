e04e771e13acb67f442e3b6aa7f348336af8bc09
Gaurav Pathak <>
2021-08-07 20:41:46 +1000

    database relationship, seeding, tinker testing

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
 
 class User extends Authenticatable
 {
@@ -40,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'author_id');
+    }
 }
